de57226ec6894392ffb0d619a29565f1e561117a
Robin Bowes <robin.bowes@yo61.com>
2006-12-19 23:38:30 +0000

    Add some notes to makerelease script

--- utils/makerelease.pl
+++ utils/makerelease.pl
@@ -3,4 +3,17 @@
 use warnings;
 use strict;
 
+# utility to create a release
 
+# Steps:
+#
+# Pre-release work:
+# ================
+#  - Update change log
+#  - tag release in subversion
+#
+# Building a release
+# ==================
+#  - export release tag into temp dir
+#  - create tarball and gzip
+#  - copy to download directory
