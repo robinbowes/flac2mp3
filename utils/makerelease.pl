5f22250ab5f08494b427908c92b07ef2bd87f2dc
Robin Bowes <robin.bowes@yo61.com>
2007-01-07 23:53:41 +0000

    Changes to "makerelease" script

--- utils/makerelease.pl
+++ utils/makerelease.pl
@@ -11,9 +11,18 @@ use strict;
 # ================
 #  - Update change log
 #  - tag release in subversion
+#    svn copy trunk tags/RELEASE-x.y.z
 #
 # Building a release
 # ==================
 #  - export release tag into temp dir
+#    svn export \
+        http://robinbowes.com/svn/flac2mp3/tags/RELEASE-x.y.z \
+        flac2mp3-x.y.z
+#  - remove some dirs from the export so they aren't packaged
+        rm -rf flac2mp3-x.y.z/patches
 #  - create tarball and gzip
+        tar cvzf flac2mp3-x.y.z.tar.gz flac2mp3-x.y.z
+#  - create zip file (for windows users)
+        zip -r9 flac2mp3-x.y.z.zip flac2mp3-x.y.z
 #  - copy to download directory
