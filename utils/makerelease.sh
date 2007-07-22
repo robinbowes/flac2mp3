c2c4db8d17ec6a3ad1f8756413faca82ebc94234
Robin Bowes <robin.bowes@yo61.com>
2007-07-22 11:43:07 +0000

    Fix makerelease script

--- utils/makerelease.sh
+++ utils/makerelease.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-use warnings;
-use strict;
-
 # utility to create a release
 
 # Steps:
@@ -14,22 +11,22 @@ use strict;
 #    svn copy trunk tags/RELEASE-x.y.z
 #
 
-export $VER=0.3.0rc1
+export VER="0.3.0rc1"
 
 # Building a release
 # ==================
 #  - export release tag into temp dir
     svn export \
-        http://robinbowes.com/svn/flac2mp3/tags/RELEASE-$VER \
-        flac2mp3-$VER
+        http://robinbowes.com/svn/flac2mp3/tags/RELEASE-${VER} \
+        flac2mp3-${VER} &&
 #  - remove some dirs from the export so they aren't packaged
-        rm -rf flac2mp3-$VER/patches flac2mp3-$VER/utils
+        rm -rf flac2mp3-${VER}/patches flac2mp3-${VER}/utils &&
 #  - create tarball and gzip
-        tar cvzf flac2mp3-$VER.tar.gz flac2mp3-$VER
+        tar cvzf flac2mp3-${VER}.tar.gz flac2mp3-${VER} &&
 #  - create zip file (for windows users)
-        zip -r9 flac2mp3-$VER.zip flac2mp3-$VER
+        zip -r9 flac2mp3-${VER}.zip flac2mp3-${VER} &&
 #  - copy to download directory
 	sudo cp \
-		flac2mp3-$VER.tar.gz \
-		flac2mp3-$VER.zip \
+		flac2mp3-${VER}.tar.gz \
+		flac2mp3-${VER}.zip \
 		/var/www/sites/robinbowes.com/projects/download/flac2mp3/
