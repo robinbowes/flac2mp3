7356173581014bbebffcf03429d8f59f0e6e6110
Robin Bowes <robin.bowes@yo61.com>
2009-05-01 22:11:10 +0000

    Update Text::Glob to 0.08

--- lib/Text/Glob.pm
+++ lib/Text/Glob.pm
@@ -1,182 +1,190 @@
-package Text::Glob;
+package File::Glob;
+
 use strict;
-use Exporter;
-use vars qw/$VERSION @ISA @EXPORT_OK
-            $strict_leading_dot $strict_wildcard_slash/;
-$VERSION = '0.07';
-@ISA = 'Exporter';
-@EXPORT_OK = qw( glob_to_regex match_glob );
+our($VERSION, @ISA, @EXPORT_OK, @EXPORT_FAIL, %EXPORT_TAGS,
+    $AUTOLOAD, $DEFAULT_FLAGS);
 
-$strict_leading_dot    = 1;
-$strict_wildcard_slash = 1;
+use XSLoader ();
 
-use constant debug => 0;
+@ISA = qw(Exporter);
 
-sub glob_to_regex {
-    my $glob = shift;
-    my ($regex, $in_curlies, $escaping);
-    local $_;
-    my $first_byte = 1;
-    for ($glob =~ m/(.)/gs) {
-        if ($first_byte) {
-            if ($strict_leading_dot) {
-                $regex .= '(?=[^\.])' unless $_ eq '.';
-            }
-            $first_byte = 0;
-        }
-        if ($_ eq '/') {
-            $first_byte = 1;
-        }
-        if ($_ eq '.' || $_ eq '(' || $_ eq ')' || $_ eq '|' ||
-            $_ eq '+' || $_ eq '^' || $_ eq '$' || $_ eq '@' || $_ eq '%' ) {
-            $regex .= "\\$_";
-        }
-        elsif ($_ eq '*') {
-            $regex .= $escaping ? "\\*" :
-              $strict_wildcard_slash ? "[^/]*" : ".*";
-        }
-        elsif ($_ eq '?') {
-            $regex .= $escaping ? "\\?" :
-              $strict_wildcard_slash ? "[^/]" : ".";
-        }
-        elsif ($_ eq '{') {
-            $regex .= $escaping ? "\\{" : "(";
-            ++$in_curlies unless $escaping;
-        }
-        elsif ($_ eq '}' && $in_curlies) {
-            $regex .= $escaping ? "}" : ")";
-            --$in_curlies unless $escaping;
-        }
-        elsif ($_ eq ',' && $in_curlies) {
-            $regex .= $escaping ? "," : "|";
-        }
-        elsif ($_ eq "\\") {
-            if ($escaping) {
-                $regex .= "\\\\";
-                $escaping = 0;
-            }
-            else {
-                $escaping = 1;
-            }
-            next;
-        }
-        else {
-            $regex .= $_;
-            $escaping = 0;
-        }
-        $escaping = 0;
+# NOTE: The glob() export is only here for compatibility with 5.6.0.
+# csh_glob() should not be used directly, unless you know what you're doing.
+
+@EXPORT_OK   = qw(
+    csh_glob
+    bsd_glob
+    glob
+    GLOB_ABEND
+    GLOB_ALPHASORT
+    GLOB_ALTDIRFUNC
+    GLOB_BRACE
+    GLOB_CSH
+    GLOB_ERR
+    GLOB_ERROR
+    GLOB_LIMIT
+    GLOB_MARK
+    GLOB_NOCASE
+    GLOB_NOCHECK
+    GLOB_NOMAGIC
+    GLOB_NOSORT
+    GLOB_NOSPACE
+    GLOB_QUOTE
+    GLOB_TILDE
+);
+
+%EXPORT_TAGS = (
+    'glob' => [ qw(
+        GLOB_ABEND
+	GLOB_ALPHASORT
+        GLOB_ALTDIRFUNC
+        GLOB_BRACE
+        GLOB_CSH
+        GLOB_ERR
+        GLOB_ERROR
+        GLOB_LIMIT
+        GLOB_MARK
+        GLOB_NOCASE
+        GLOB_NOCHECK
+        GLOB_NOMAGIC
+        GLOB_NOSORT
+        GLOB_NOSPACE
+        GLOB_QUOTE
+        GLOB_TILDE
+        glob
+        bsd_glob
+    ) ],
+);
+
+$VERSION = '1.06';
+
+sub import {
+    require Exporter;
+    my $i = 1;
+    while ($i < @_) {
+	if ($_[$i] =~ /^:(case|nocase|globally)$/) {
+	    splice(@_, $i, 1);
+	    $DEFAULT_FLAGS &= ~GLOB_NOCASE() if $1 eq 'case';
+	    $DEFAULT_FLAGS |= GLOB_NOCASE() if $1 eq 'nocase';
+	    if ($1 eq 'globally') {
+		local $^W;
+		*CORE::GLOBAL::glob = \&File::Glob::csh_glob;
+	    }
+	    next;
+	}
+	++$i;
     }
-    print "# $glob $regex\n" if debug;
-    qr/^$regex$/;
+    goto &Exporter::import;
 }
 
-sub match_glob {
-    print "# ", join(', ', map { "'$_'" } @_), "\n" if debug;
-    my $glob = shift;
-    my $regex = glob_to_regex $glob;
-    local $_;
-    grep { $_ =~ $regex } @_;
+sub AUTOLOAD {
+    # This AUTOLOAD is used to 'autoload' constants from the constant()
+    # XS function.  If a constant is not found then control is passed
+    # to the AUTOLOAD in AutoLoader.
+
+    my $constname;
+    ($constname = $AUTOLOAD) =~ s/.*:://;
+    my ($error, $val) = constant($constname);
+    if ($error) {
+	require Carp;
+	Carp::croak($error);
+    }
+    eval "sub $AUTOLOAD { $val }";
+    goto &$AUTOLOAD;
+}
+
+XSLoader::load 'File::Glob', $VERSION;
+
+# Preloaded methods go here.
+
+sub GLOB_ERROR {
+    return (constant('GLOB_ERROR'))[1];
+}
+
+sub GLOB_CSH () {
+    GLOB_BRACE()
+	| GLOB_NOMAGIC()
+	| GLOB_QUOTE()
+	| GLOB_TILDE()
+	| GLOB_ALPHASORT()
+}
+
+$DEFAULT_FLAGS = GLOB_CSH();
+if ($^O =~ /^(?:MSWin32|VMS|os2|dos|riscos|MacOS)$/) {
+    $DEFAULT_FLAGS |= GLOB_NOCASE();
+}
+
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
+sub bsd_glob {
+    my ($pat,$flags) = @_;
+    $flags = $DEFAULT_FLAGS if @_ < 2;
+    return doglob($pat,$flags);
+}
+
+# File::Glob::glob() is deprecated because its prototype is different from
+# CORE::glob() (use bsd_glob() instead)
+sub glob {
+    splice @_, 1; # don't pass PL_glob_index as flags!
+    goto &bsd_glob;
+}
+
+## borrowed heavily from gsar's File::DosGlob
+my %iter;
+my %entries;
+
+sub csh_glob {
+    my $pat = shift;
+    my $cxix = shift;
+    my @pat;
+
+    # glob without args defaults to $_
+    $pat = $_ unless defined $pat;
+
+    # extract patterns
+    $pat =~ s/^\s+//;	# Protect against empty elements in
+    $pat =~ s/\s+$//;	# things like < *.c> and <*.c >.
+			# These alone shouldn't trigger ParseWords.
+    if ($pat =~ /\s/) {
+        # XXX this is needed for compatibility with the csh
+	# implementation in Perl.  Need to support a flag
+	# to disable this behavior.
+	require Text::ParseWords;
+	@pat = Text::ParseWords::parse_line('\s+',0,$pat);
+    }
+
+    # assume global context if not provided one
+    $cxix = '_G_' unless defined $cxix;
+    $iter{$cxix} = 0 unless exists $iter{$cxix};
+
+    # if we're just beginning, do it all first
+    if ($iter{$cxix} == 0) {
+	if (@pat) {
+	    $entries{$cxix} = [ map { doglob($_, $DEFAULT_FLAGS) } @pat ];
+	}
+	else {
+	    $entries{$cxix} = [ doglob($pat, $DEFAULT_FLAGS) ];
+	}
+    }
+
+    # chuck it all out, quick or slow
+    if (wantarray) {
+        delete $iter{$cxix};
+        return @{delete $entries{$cxix}};
+    }
+    else {
+        if ($iter{$cxix} = scalar @{$entries{$cxix}}) {
+            return shift @{$entries{$cxix}};
+        }
+        else {
+            # return undef for EOL
+            delete $iter{$cxix};
+            delete $entries{$cxix};
+            return undef;
+        }
+    }
 }
 
 1;
 __END__
 
-=head1 NAME
-
-Text::Glob - match globbing patterns against text
-
-=head1 SYNOPSIS
-
- use Text::Glob qw( match_glob glob_to_regex );
-
- print "matched\n" if match_glob( "foo.*", "foo.bar" );
-
- # prints foo.bar and foo.baz
- my $regex = glob_to_regex( "foo.*" );
- for ( qw( foo.bar foo.baz foo bar ) ) {
-     print "matched: $_\n" if /$regex/;
- }
-
-=head1 DESCRIPTION
-
-Text::Glob implements glob(3) style matching that can be used to match
-against text, rather than fetching names from a filesystem.  If you
-want to do full file globbing use the File::Glob module instead.
-
-=head2 Routines
-
-=over
-
-=item match_glob( $glob, @things_to_test )
-
-Returns the list of things which match the glob from the source list.
-
-=item glob_to_regex( $glob )
-
-Returns a compiled regex which is the equiavlent of the globbing
-pattern.
-
-=back
-
-=head1 SYNTAX
-
-The following metacharacters and rules are respected.
-
-=over
-
-=item C<*> - match zero or more characters
-
-C<a*> matches C<a>, C<aa>, C<aaaa> and many many more.
-
-=item C<?> - match exactly one character
-
-C<a?> matches C<aa>, but not C<a>, or C<aa>
-
-=item Character sets/ranges
-
-C<example.[ch]> matches C<example.c> and C<example.h>
-
-C<demo.[a-c]> matches C<demo.a>, C<demo.b>, and C<demo.c>
-
-=item alternation
-
-C<example.{foo,bar,baz}> matches C<example.foo>, C<example.bar>, and
-C<example.baz>
-
-=item leading . must be explictly matched
-
-C<*.foo> does not match C<.bar.foo>.  For this you must either specify
-the leading . in the glob pattern (C<.*.foo>), or set
-C<$Text::Glob::strict_leading_dot> to a false value while compiling
-the regex.
-
-=item C<*> and C<?> do not match /
-
-C<*.foo> does not match C<bar/baz.foo>.  For this you must either
-explicitly match the / in the glob (C<*/*.foo>), or set
-C<$Text::Glob::strict_wildcard_slash> to a false value with compiling
-the regex.
-
-=back
-
-=head1 BUGS
-
-The code uses qr// to produce compiled regexes, therefore this module
-requires perl version 5.005_03 or newer.
-
-=head1 AUTHOR
-
-Richard Clamp <richardc@unixbeard.net>
-
-=head1 COPYRIGHT
-
-Copyright (C) 2002 Richard Clamp.  All Rights Reserved.
-
-This module is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-L<File::Glob>, glob(3)
-
-=cut
